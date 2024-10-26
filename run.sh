f8009fd00ab0376aed9ada8d20ea5a039201f2af
anthonyceponis <anthonyceponis@gmail.com>
2024-10-26 20:07:28 +0100

    First commit

---
+++ run.sh
@@ -0,0 +1,3 @@
+npx tailwindcss -i ./src/styles.css -o ./public/tailwind.css 
+dune build 
+dune exec _build/default/bin/main.exe 
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,ml}",      // Includes all .html, .js, and .ml files in src/
+    "./public/**/*.html",            // Includes HTML files in the public folder
+    "./bin/**/*.{html,js,ml}",       // Includes all .html, .js, and .ml files in bin/
+    "./app.ml",                      // Any specific files directly in the root
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Space Mono', 'monospace'], // Set Space Mono as the default sans-serif font
+      },
+    },
+  },
+  plugins: [],
+}
+
